9cc7d25fa9d09c611afddc1d88410abfc4febcaf
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-07-25 19:02:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,6 @@
 def turn_count(board)
   counter = 0
   board.drop(" ").each do { |element| counter += element.count }
-    if element == "X" || element == "O"
-      counter += 1
-    end
   end
 end
 

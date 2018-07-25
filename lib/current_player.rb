5bad2b07ea5fc374d779ba4c3f45f06ec80e7465
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-07-25 19:02:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0
-  board.drop(" ").each do { |element| counter += element.count }
+  board.drop(" ").each do { |element| return counter += element.count }
   end
 end
 
